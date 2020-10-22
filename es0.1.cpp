9359275ff661ac53d484f805734ea74ba1d2915a
Guadagnin Marina (ACC0144855) <marina.guadagnin@sci-b106pc079x.unitn.it>
2020-10-22 15:33:36 +0200

    esercizio 1 e 2

---
+++ es0.1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    int y;
+    int z;
+    cout <<"inserire numero" << endl;
+    cin>>x;
+    cin>>y;
+    cin>>z;
+    cout<<"risultato"<<x*y*z;
+}
